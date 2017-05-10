9b4f9cc2cb64d3182ffc8c92655e65f3ea3f4e71
mlorsini <marie-lou.orsini@insa-lyon.fr>
2017-05-10 09:07:22 +0200

    autentification +deco

--- INSport/tableaubord/views.py
+++ INSport/tableaubord/views.py
@@ -24,7 +24,7 @@ def sign1(request):
 		user = authenticate(username=username, password=password)
 		if user:
 			login(request, user)  # nous connectons l'utilisateur
-			return redirect('sign2')
+			return redirect('Uilisateur')
 
 	return render(request, 'sign1.html',locals())
 

7dafeb1dba5ca448e51a807c02acd7fb3960e2db
Flavien Marty <flav.marty@gmail.com>
2017-05-14 15:02:05 +0200

    Bouton dislike eevent

--- INSport/evenement/urls.py
+++ INSport/evenement/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.evenement),
     url(r'^events/(?P<id>\d+)$', views.detailevenement,name='detailEvenement'),
+    url(r'^dislikeEvt/(?P<id>\d+)$', views.dislikeEvent,name='dislikeEvent'),
 ]
--- INSport/evenement/views.py
+++ INSport/evenement/views.py
@@ -42,3 +42,17 @@ def detailevenement(request,id):
 	photoEvt=detail.sports_id
 	photoSport=Sport.objects.get(pk=photoEvt)
 	return render(request,'detailEvenement.html',locals())
+
+@login_required
+def dislikeEvent(request,id):
+	event = Evenement.objects.get(pk=id)
+	part=Participation.objects.filter(evenement=event, participant= request.user)
+	part.update(participe='1')
+	try:
+		context=Evenement.objects.raw('SELECT tableaubord_evenement.id,tableaubord_evenement.description FROM tableaubord_evenement INNER JOIN tableaubord_participation ON tableaubord_evenement.id = tableaubord_participation.evenement_id WHERE tableaubord_participation.participant_id=%s AND tableaubord_participation.participe=0;',(request.user.id,))
+	except :
+		context= None
+	
+	return render(request, 'evenement.html',{'context': context})
+
+

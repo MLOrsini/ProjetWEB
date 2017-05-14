2860351244ce8a79deac5e27f7efc01479427860
MLOrsini <marie-lou.orsini@insa-lyon.fr>
2017-05-15 00:17:42 +0200

    Update urls.py

--- INSport/profil/urls.py
+++ INSport/profil/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.Utilisateur, name='Utilisateur'),
-	url('monProfil', views.monProfil)
+    url(r'^$', views.Utilisateurs, name='Utilisateur'),
+	url('monProfil', views.monProfil),
+	url(r'^delete$', views.deleteUser, name='delete')
 ]
